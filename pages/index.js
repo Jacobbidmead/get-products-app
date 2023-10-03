2a704f102478d4da87fa6decaa41315887f6035b
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-10-03 16:19:11 +0100

    Adjusted layout

--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,27 @@ const HomePage = () => {
   }, []);
   return (
     <>
-      <div className="bg-neutral-500">
+      <div className="">
         <h1 className="text-8xl text-center p-5 pt-8 text-green-400">
           Products
         </h1>
         <div className="grid grid-cols-4 gap-5 p-10">
           {products.map((product) => (
             <div key={product.id} className="flex justify-center flex-col">
-              <img src={product.thumbnail} alt={product.name} width={100} />
-              <h2>{product.title}</h2>
-              <p className="">{product.description}</p>
-              <Link href={`/product/${product.id}`}>View Product</Link>
+              <div>
+                <img src={product.thumbnail} alt={product.name} width={100} />
+              </div>
+              <div>
+                <h2>{product.title}</h2>
+              </div>
+              <div>
+                {" "}
+                <p className="">{product.description}</p>
+              </div>{" "}
+              <div>
+                {" "}
+                <Link href={`/product/${product.id}`}>View Product</Link>
+              </div>
             </div>
           ))}
         </div>
