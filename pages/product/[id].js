0f42d3afa1f04e789de58bd85f5afa7e95db05a7
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-10-03 15:43:00 +0100

    Added logic to populate product by id
    Images not loading

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,7 +2,32 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 const ProductPage = () => {
-  return;
+  const router = useRouter();
+  const { id } = router.query;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      fetch(`https://dummyjson.com/products/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setProduct(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching the product:", error);
+        });
+    }
+  }, [id]);
+
+  if (!product) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      <img src={product.image} alt={product.name} width={100} height={100} />
+      <p>{product.description}</p>
+    </div>
+  );
 };
 
 export default ProductPage;
